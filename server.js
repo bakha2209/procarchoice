be1db53e38dfb42433f5cbd4f1514eee6222e324
bakha2209 <bakhodir2209@gmail.com>
2024-01-15 16:37:29 +0500

    implementing express part of the project

---
+++ server.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+
+const http = require("http");
+const mongodb = require("mongodb")
+
+
+const connectionString = process.env.MONGO_URL;
+mongodb.connect(connectionString,{
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+}, (err,client)=> {
+
+    if(err) console.log("Error on connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed")
+        module.exports = client;
+        
+        const app = require("./app")
+        const server = http.createServer(app);
+        let PORT = process.env.PORT || 3001;
+        server.listen(PORT, function () {
+          console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        });  
+    }
+});
